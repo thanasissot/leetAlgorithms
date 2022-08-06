a4df89c174a09df50bc7e2f2f3c3cda0a57e3638
thanasissot <thanasissot1@gmail.com>
2022-08-06 13:37:31 +0300

    new batch

---
+++ src/main/java/NeedCode150/Easy/_136.java
@@ -0,0 +1,24 @@
+package NeedCode150.Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _136 {
+    // Brute force solution
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a : nums) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        
+        int ret = Integer.MIN_VALUE;
+        for (int a : map.keySet()) {
+            if (map.get(a) == 1) {
+                ret = a;
+                break;                
+            }
+        }
+        
+        return ret;
+    }
+}
---
+++ src/main/java/NeedCode150/Medium/_424.java
@@ -0,0 +1,82 @@
+package NeedCode150.Medium;
+
+import java.util.ArrayList;
+
+public class _424 {
+    public static int characterReplacement(String s, int k) {
+        // chartFrequencies
+        int max = 0, temp = 0, index = 0, next = 0, len = k;
+        boolean flag = false;
+        char[] arr = s.toCharArray();
+        while (index < arr.length - 1) {
+            char c = arr[index];
+            while (index < arr.length - 1 && c == arr[index + 1]){
+                temp++;
+                index++;
+            }
+
+            if (temp-- <= len) {
+                c = arr[index+1];
+                len -= index;
+            }
+            next = index + 1; // next index to try the loop
+
+            while (index < arr.length - 1 && (len > 0 || c == arr[index + 1])) {
+                if (c != arr[index + 1]) {
+                    len--;
+                }
+                temp++;
+                index++;
+            }
+
+            max = Math.max(max, temp);
+            index = next;
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(
+                characterReplacement("ABBB", 2)
+        );
+        System.out.println(
+                characterReplacement("ABABAA", 1)
+        );
+        System.out.println(
+                characterReplacement("ABAB", 2)
+        );
+        System.out.println(
+                characterReplacement("AABABBBA", 0)
+        );
+        System.out.println(
+                characterReplacement("AABABBA", 2)
+        );
+
+
+    }
+
+
+//    int next = 0;
+//    int max = 0, temp = 0;
+//    int len = k;
+//    int index = 0;
+//    char c;
+//    char[] arr = s.toCharArray();
+//        while (index < arr.length) {
+//        next = index+1;
+//        c = arr[index];
+//        while(index < arr.length && (c == arr[index] || len > 0)){
+//            if (c != arr[index]) {
+//                len--;
+//            }
+//            index++;
+//            temp++;
+//        }
+//        max = Math.max(max, temp);
+//        temp = 0;
+//        len = k;
+//        index = next;
+//    }
+//        return max;
+}
