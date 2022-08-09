8f8b8f36c3e4a0bf2fffab5807689a13da4d8f27
thanasissot <thanasissot1@gmail.com>
2022-08-09 20:23:46 +0300

    new batch

---
+++ src/main/java/NeedCode150/Easy/_118.java
@@ -0,0 +1,28 @@
+package NeedCode150.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _118 {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> outer = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            outer.add(new ArrayList<>());
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == i - 1) {
+                    outer.get(i).add(1);
+                    continue;
+                }
+
+                outer.get(i).add(
+                        outer.get(i - 1).get(j - 1) + outer.get(i - 1).get(j)
+                );
+            }
+        }
+        outer.remove(0);
+        return outer;
+
+    }
+
+}
---
+++ src/main/java/NeedCode150/Medium/_2.java
@@ -0,0 +1,61 @@
+package NeedCode150.Medium;
+
+import NeedCode150.InnerClasses.ListNode;
+
+public class _2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode temp = head;
+
+        int cout = 0;
+        int sum = Integer.MIN_VALUE;
+        while (l1 != null && l2 != null) {
+            sum = l1.val + l2.val + cout;
+            if (sum >= 10) {
+                cout = 1;
+                sum %= 10;
+            }
+            else {
+                cout = 0;
+            }
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            sum = l1.val + cout;
+            if (sum >= 10) {
+                cout = 1;
+                sum %= 10;
+            }
+            else {
+                cout = 0;
+            }
+            l1 = l1.next;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+        }
+
+        while (l2 != null) {
+            sum = l2.val + cout;
+            if (sum >= 10) {
+                cout = 1;
+                sum %= 10;
+            }
+            else {
+                cout = 0;
+            }
+            l2 = l2.next;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+        }
+
+        if (cout == 1) {
+            temp.next = new ListNode(1);
+        }
+        return head.next;
+    }
+}
+
