80df6d63b7d09a4455cd75c866ba67098a779198
thanasissot <thanasissot1@gmail.com>
2022-08-09 23:42:09 +0300

    one more

---
+++ src/main/java/NeedCode150/Easy/_202.java
@@ -0,0 +1,34 @@
+package NeedCode150.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _202 {
+
+    public static boolean isHappy(int n) {
+        if (n == 1) return true;
+        int fast = n;
+        int slow = n;
+        while ( slow != 1 && fast != 1 ) {
+            slow = getSquaredDigitsSum(slow);
+            fast = getSquaredDigitsSum(getSquaredDigitsSum(fast));
+            if (slow ==  fast && slow != 1 && fast != 1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int getSquaredDigitsSum(int n) {
+        int sum = 0;
+        while ( n > 0 ) {
+            sum += (int) Math.pow(n%10, 2);
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+    }
+}
