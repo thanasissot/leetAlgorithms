650ede030d071bd69c3e9b8358cd34eda44ace67
thanasissot <thanasissot1@gmail.com>
2022-08-09 21:58:34 +0300

    easy but medium panindrome linked list O(n) time, O(1) space

---
+++ src/main/java/NeedCode150/Easy/_234.java
@@ -0,0 +1,43 @@
+package NeedCode150.Easy;
+
+import NeedCode150.InnerClasses.ListNode;
+
+public class _234 {
+    public boolean isPalindrome(ListNode head) {
+        // get linked list length
+        ListNode temp = head;
+        int len = 0;
+        while (head != null) {
+            head = head.next;
+            len++;
+        }
+        // reverse the 2nd half of the linked list
+        // take into account even and odd length
+        if (len % 2 == 1) {
+            len += 1;
+        }
+        len /= 2;
+        head = temp;
+        //
+        for (int i = 0; i < len; i++) temp = temp.next;
+        ListNode prev = null;
+        ListNode current = temp;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        temp = prev;
+
+        // check palindrome
+        while (head != null && temp != null && head != temp) {
+            if (head.val != temp.val) return false;
+            head = head.next;
+            temp = temp.next;
+        }
+
+        return true;
+    }
+}
