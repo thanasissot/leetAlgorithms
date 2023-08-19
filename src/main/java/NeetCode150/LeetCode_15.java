035f716f5dada5ab358e6abdfc7db945b38f4bc4
thanasissot <thanasissot1@gmail.com>
2023-08-19 10:31:42 +0300

    optimized solution for 15

--- src/main/java/NeetCode150/LeetCode_15.java
+++ src/main/java/NeetCode150/LeetCode_15.java
@@ -35,6 +35,46 @@ public class LeetCode_15 {
         return new ArrayList<>(map.values());
     }
 
+    public List<List<Integer>> threeSum1(int[] nums) {
+        Arrays.sort(nums);
+        List< List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i <= nums.length - 2; i++) {
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    // Skip duplicate elements for left
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    // Skip duplicate elements for right
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    // Move the pointers
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         new LeetCode_15().threeSum(new int[]{0,0,0});
     }
